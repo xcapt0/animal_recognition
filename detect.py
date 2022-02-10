1f28372b0d8e0f385a3096260e76b58b4c9c3964
Vadim <townhor@mail.ru>
2022-02-10 15:54:27 +0500

    Added source choosing

--- detect.py
+++ detect.py
@@ -1,11 +1,12 @@
 import torch
 import cv2
 import numpy as np
+from argparse import ArgumentParser
 
 
-def detect():
+def detect(args):
     model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights/best.pt', force_reload=True)
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(args.source)
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -22,4 +23,12 @@ def detect():
 
 
 if __name__ == '__main__':
-    detect()
+    parser = ArgumentParser()
+    parser.add_argument('--source', type=str, required=True,
+                        help='Source to load the video from webcam or mp4/avi file')
+    user_args = parser.parse_args()
+
+    if user_args.source.isdigit():
+        user_args.source = int(user_args.source)
+
+    detect(user_args)
